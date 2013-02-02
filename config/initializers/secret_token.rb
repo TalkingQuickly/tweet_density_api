edd839c8812e8d0be45be25f89d2fc451d8c2d6c
Ben Dixon <ben@hillsbede.co.uk>
2013-02-02 12:08:12 +0000

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tstream::Application.config.secret_token = 'c39c62cc59efad311fd6c3c642491f3c6b4fd6b4b117f4e5c779b8c851094440cc35c40240badeb2c4b0bfb283a589c24f4fe39665c2724f79385388ce942bd0'
