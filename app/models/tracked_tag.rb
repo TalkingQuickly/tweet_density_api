1f07795e89f0c553e087d748d74a1614c175f9f7
Ben Dixon <ben@hillsbede.co.uk>
2013-02-03 14:16:34 +0000

    randing on difficulty

--- app/models/tracked_tag.rb
+++ app/models/tracked_tag.rb
@@ -30,7 +30,7 @@ class TrackedTag < ActiveRecord::Base
     lt = self.get_density(5000)
 
     if st > 0 && lt > 0
-      return ((st/lt) * 100)/2
+      return (((st/lt) * 100)/2).to_i
     else
       return 50
     end
